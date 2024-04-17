91d6dcee2664e28275977fe2744294d438611a67
Lucas Rodrigues <lucas.rcaetano1122@gmail.com>
2024-04-17 16:54:28 -0300

    Remove unused lines

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,16 +2,6 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-/*const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};*/
-
 const firebaseConfig: FirebaseOptions = {
     apiKey: "AIzaSyC_MfdsPV9oA_t7f7BvU--zbv-khVm2GI0",
     authDomain: "projetointegrador-testes.firebaseapp.com",
