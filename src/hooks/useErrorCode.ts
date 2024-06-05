3365a13fa83a5054e7ee3cd2430264e3418f63c7
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2024-06-05 16:51:38 -0300

    Error Code pages

--- src/hooks/useErrorCode.ts
+++ src/hooks/useErrorCode.ts
@@ -1,26 +1,32 @@
 type Error = {
-    code: number,
     title: string,
     message: string
 }
 
 export default function useErrorCode(code: number){    
     const ErrorList: {[key: string]: Error} = { 
+        403: {
+            title: "Acesso negado",
+            message: "Desculpe, mas parece que voce não tem permissão para acessar esta página."
+        },
         404: {
-            code: 404,
             title: "Página não encontrada",
             message: "Desculpe, mas parece que a página que tentou acessar não existe."
         },
         500: {
-            code: 500,
             title: "Erro interno",
             message: "Desculpe-nos, algo de errado não está certo. :("
+        },
+        503: {
+            title: "Servidor indisponível",
+            message: "Desculpe-nos, o servidor parece estar indisponível. :("
         }
     }
 
     const error = ErrorList[code as keyof typeof ErrorList];
 
     if(error == undefined)
-        return ErrorList[500];
+        window.location.href = '/error/404';
+        return ErrorList[404];
     return error;
 }
--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -14,11 +14,7 @@ export default function ErrorPage(){
                 <h1>{error.title}</h1>
                 <hr />
                 <div id="error-data">
-                    <h2>Código do erro: <i id="error-code">{ErrorCode}</i></h2>
-                    <h3>Descrição do erro: </h3>
-                    <div id="description">
-                        {error.message}
-                    </div>
+                    {error.message}
                 </div>
                 <h4>Para voltar para a tela inicial, <Link to="/">clique aqui</Link>.</h4>
             </div>
