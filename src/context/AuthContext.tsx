a16ca6b46976fb459fd9d0be833d159dd2cec05b
Lucas Rodrigues <lucas.rcaetano1122@gmail.com>
2024-06-09 17:30:37 -0300

    feat: auth context database

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,8 @@
-import { ReactNode, createContext } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+import Database from '../services/database';
+import Auth from '../services/auth';
 
 type AuthContextProps = {
     children: ReactNode,
@@ -8,7 +12,16 @@ type User = {
     uid: string, 
     email: string, 
     name: string,
+    avatar: string | undefined,
     
+    stats: {
+        level: 1,
+        xp: 0,
+        coins: 0
+    },
+    inventory: string[],
+    tasks: string[],
+    goals: string[]
 }
 
 type ContextProps = {
@@ -20,8 +33,66 @@ type ContextProps = {
 export const authContext = createContext({} as ContextProps);
 
 export default function AuthContext(props: AuthContextProps){
+    const [loadingUser, setLoadingUser] = useState<boolean>(true);
+    const [user, setUser] = useState<User>({} as User);
+
     const { children } = props;
-    
+
+    const { getAuth } = Auth;
+    const { ref, once } = Database;
+
+    useEffect(() => {
+        setLoadingUser(true);
+        const unsubscribe = onAuthStateChanged(getAuth(), user => {
+            if(user){
+                const uid = user.uid
+
+                const users = ref('users')
+                // Verificar se existe o usuario no banco de dados
+                once(users, snapshot => {
+                    if(snapshot.child(uid).exists()){
+                        const data = snapshot.child(uid).val();
+                        setUser({
+                            uid: data.uid,
+                            email: data.email,
+                            name: data.name,
+                            avatar: data.avatar,
+
+                            stats: {
+                                coins: data.stats.coins,
+                                xp: data.stats.xp,
+                                level: data.stats.level
+                            },
+                            inventory: data.inventory,
+                            tasks: data.tasks,
+                            goals: data.goals
+                        })
+                    } else {
+                        setUser({
+                            uid: user.uid,
+                            email: user.email!,
+                            name: user.displayName || "Jacaré Banguela",
+                            avatar: undefined, // 
+
+                            stats: {
+                                coins: 0,
+                                xp: 0,
+                                level: 1
+                            },
+                            inventory: [],
+                            tasks: [], 
+                            goals: [] // metas de boas vindas
+                        })
+                    }
+
+                    setLoadingUser(false);
+                })
+            }
+        })
+
+        return unsubscribe;
+    }, [])
+
     return (
         <authContext.Provider value={{} as ContextProps}>
             {children}
--- src/services/database.ts
+++ src/services/database.ts
@@ -1,14 +1,20 @@
-import { child, get, ref, set, update } from 'firebase/database';
+import { DataSnapshot, Database, child, get, onValue, ref, set, update, push, ThenableReference, DatabaseReference } from 'firebase/database';
 
 import { DatabaseInstance } from './firebase';
 
 export default {
-    ref: function(path: string){
-        return ref(DatabaseInstance, path)
+    ref: function(path?: string){
+        if(path && path != "")
+            return ref(DatabaseInstance, path)
+        else
+            return ref(DatabaseInstance)
     },
 
-    set: async function(path: string, value: unknown){
-        return set(ref(DatabaseInstance, path), value)
+    set: async function(path: string | ThenableReference | DatabaseReference , value: unknown){
+        if (typeof path === 'string')
+            return set(ref(DatabaseInstance, path), value)
+        else
+            return set(path, value)
     },
 
     get: get,
@@ -19,5 +25,25 @@ export default {
     
     child: function(path: string){
         return child(ref(DatabaseInstance), path)
+    },
+
+    once: function(path: string | DatabaseReference, callback: (snapshot: DataSnapshot) => unknown, callbackErr?: (error: Error) => unknown){
+        if(callbackErr)
+            if(typeof path === 'string')
+                return onValue(ref(DatabaseInstance, path), callback, callbackErr, {onlyOnce: true})
+            else
+                return onValue(path, callback, callbackErr, {onlyOnce: true})
+        else
+            if (typeof path === 'string')
+                return onValue(ref(DatabaseInstance, path), callback, {onlyOnce: true})
+            else
+                return onValue(path, callback, {onlyOnce: true})
+    },
+
+    push: function(path: string, value?: unknown){
+        if(value)
+            return push(ref(DatabaseInstance, path), value);
+        else
+            return push(ref(DatabaseInstance, path));
     }
 }
