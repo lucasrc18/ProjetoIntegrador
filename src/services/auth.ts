e1a38a09ce961f92c9964d8d014f0febdf901ab1
Lucas Rodrigues <lucas.rcaetano1122@gmail.com>
2024-06-09 17:42:55 -0300

    Auth ts refactor

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,49 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import Database from './database';
 import { AuthInstance } from './firebase';
 
-type UserAuthType = {
-    email: string,
-    password: string,
-    username: string
-};
-
-type UserEmailType = {
-    email: string
-}
-
-const navigate = useNavigate();
-
-async function registerUser(user: User, user_auth: UserAuthType | UserEmailType | null = null) {
-    const { set } = Database;
-    
-    if(!user.email)
-        throw new Error("User must have an valid email");
-    
-    if(!user_auth)
-        user_auth = { email: user.email }
-
-    await set(`users/${user.uid}`, {
-        data: {
-            uid: user.uid,
-            ...user_auth
-        },
-        stats: {
-            level: 1,
-            xp: 0,
-            //hp: 50,
-            //maxHp: 50,
-            coins: 0
-        },
-        inventory: [],
-        tasks: [],
-        goals: []
-    }).then(
-        () => navigate('/profile')
-    ).catch(
-        err => navigate('/error', { state: err })
-    );
-}
-
 export default {
     /**
      *  Faz o login do usuario utilizando sua conta Google atráves do firebase
@@ -55,9 +12,12 @@ export default {
     signInWithGoogle: async () => {
         const provider = new GoogleAuthProvider();
         
-        await signInWithPopup(AuthInstance, provider).then(
-            (result) => registerUser(result.user)
-        ).catch(err => navigate('/error', { state: err }));
+        await signInWithPopup(AuthInstance, provider).then(() => {
+            window.location.href = "/"
+        }
+        ).catch(err => {
+            window.location.href = "/error/500"
+        });
     },
     /**
      *  Faz o login do usuario utilizando sua conta GitHub atráves do firebase
@@ -66,19 +26,11 @@ export default {
      */
     signInWithGithub: async () => {
         const provider = new GithubAuthProvider();
-        await signInWithPopup(AuthInstance, provider).then(
-            (result) => registerUser(result.user)
-        ).catch(err => navigate('/error', { state: err }));
-    },
-    /**
-     *  Faz o login do usuario utilizando seu email e senha conta GitHub atráves do firebase
-     *  @params null
-     *  @returns Promise<UserCredential>
-     */
-    signInWithEmailAndPassword: async ({email, password, username}: UserAuthType) => {
-        await signInWithEmailAndPassword(AuthInstance, email, password).then(
-            (result) => registerUser(result.user, {email, password, username})
-        ).catch(err => navigate('/error', { state: err }));
+        await signInWithPopup(AuthInstance, provider).then(() => {
+            window.location.href = "/"
+        }).catch(err => {
+            window.location.href = "/error/500"
+        });
     },
     /**
      *  Faz o logout do usuario
@@ -86,10 +38,11 @@ export default {
      *  @returns Promise<void>
      */
     signOut: async () => {
-        await signOut(AuthInstance).then(
-            response => navigate('/register')
-        ).catch(
-            err => navigate('/error', { state: err })
+        await signOut(AuthInstance).then(() => {
+            window.location.href = "/"
+        }).catch(() => {
+            window.location.href = "/error/500"
+        }
         );
     },
     
